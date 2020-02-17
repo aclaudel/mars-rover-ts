1807fa92b70a18da77eee002a15800fee8697049
aclaudel <arnaud.claudel@codurance.com>
2020-02-17 15:09:55 +0000

    Mars rover should move forward

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,8 +1,12 @@
 import start from "./app";
 
 describe("Mars rover", () => {
-    it("should move forward twice", () => {
-        const finalLocation = start('0 0 N', ['F']);
+    it("should move forward", () => {
+        let finalLocation;
+        finalLocation = start('0 0 N', ['M']);
         expect(finalLocation).toBe('1 0 N');
+
+        finalLocation = start('0 0 N', ['M', 'M']);
+        expect(finalLocation).toBe('2 0 N');
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,3 @@
 export default function start(initialLocation: string, commands: string[]) {
-
+    return `${commands.length} 0 N`;
 }
