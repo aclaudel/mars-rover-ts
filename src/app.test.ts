83459bfcbfcb7578dd80b4fe2b56646be6743b4b
aclaudel <arnaud.claudel@codurance.com>
2020-02-17 15:17:54 +0000

    Mars rover should turn right

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,12 +1,20 @@
-import start from "./app";
+import start, {Commands} from "./app";
+
+function startAndAssert(initialLocation: string,
+                        commands: Commands,
+                        expectedFinalLocation: string) {
+    const finalLocation = start(initialLocation, commands);
+    expect(finalLocation).toBe(expectedFinalLocation);
+}
 
 describe("Mars rover", () => {
     it("should move forward", () => {
-        let finalLocation;
-        finalLocation = start('0 0 N', ['M']);
-        expect(finalLocation).toBe('1 0 N');
+        startAndAssert('0 0 N', ['M'], '1 0 N');
+        startAndAssert('0 0 N', ['M', 'M'], '2 0 N');
+    });
 
-        finalLocation = start('0 0 N', ['M', 'M']);
-        expect(finalLocation).toBe('2 0 N');
+    it("should turn right", () => {
+        startAndAssert('0 0 N', ['R'], '0 0 E');
+        startAndAssert('0 0 N', ['R', 'R'], '0 0 S');
     });
 });
